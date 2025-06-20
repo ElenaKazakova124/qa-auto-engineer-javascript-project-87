a1687274e975f966b783d121f1d3868de61f063a
Elena <elena.kazakova124@gmail.com>
2025-06-20 20:01:28 +0300

    fix jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.mjs$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'mjs', 'json'],
-  transformIgnorePatterns: ['node_modules/(?!(your-esm-modules)/)'],
-};
