d57f975dea7f6e2dc85b5f9fe8bb642813d8f5eb
Elena <elena.kazakova124@gmail.com>
2025-06-25 20:32:58 +0300

    SDELAITE TESTI NA KAJDOM SHAGU!!!!

--- src/diffBuilder.js
+++ src/diffBuilder.js
@@ -1,28 +1,26 @@
 const buildDiff = (obj1, obj2) => {
-  const keys = Object.keys({ ...obj1, ...obj2 })
+  const keys = Object.keys({ ...obj1, ...obj2 });
+  const sortedKeys = keys.sort((a, b) => a.localeCompare(b));
+  const diff = [];
 
-  const sortedKeys = keys.sort((a, b) => a.localeCompare(b))
-
-  const diff = []
-
-  sortedKeys.forEach(key => {
+  for (const key of sortedKeys) {
     if (!(key in obj2)) {
-      diff.push({ key, type: 'removed', value: obj1[key] })
+      diff.push({ key, type: 'removed', value: obj1[key] });
     } else if (!(key in obj1)) {
-      diff.push({ key, type: 'added', value: obj2[key] })
+      diff.push({ key, type: 'added', value: obj2[key] });
     } else if (obj1[key] !== obj2[key]) {
       diff.push({
         key,
         type: 'changed',
         oldValue: obj1[key],
         newValue: obj2[key],
-      })
+      });
     } else {
-      diff.push({ key, type: 'unchanged', value: obj1[key] })
+      diff.push({ key, type: 'unchanged', value: obj1[key] });
     }
-  })
+  }
 
-  return diff
-}
+  return diff;
+};
 
-export default buildDiff
+export default buildDiff;
